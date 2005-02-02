d3b9f1758096a926dd2962be275b69dfb1169ca3
David <David@d147aa11-f70d-cc43-991b-859307a355b7>
2005-02-02 04:22:06 +0000

    updated install.cmd to show more useful output and install to a configurable directory.
    git-svn-id: svn://localhost:3690/nantftptask/trunk@46 d147aa11-f70d-cc43-991b-859307a355b7

--- install.cmd
+++ install.cmd
@@ -1,5 +1,16 @@
 @set config=%1
 @if (%config%)==() set config=release
-@copy bin\%config%\*.dll c:\usr\local\nant-0.85-rc1\build\net-1.1.win32\nant-0.85-debug\bin\tasks
-@if (%config%)==(debug) copy bin\%config%\*.pdb c:\usr\local\nant-0.85-rc1\build\net-1.1.win32\nant-0.85-debug\bin\tasks
+
+@set nantTaskDir="c:\usr\local\nant-0.85-rc1\build\net-1.1.win32\nant-0.85-debug\bin\tasks\"
+
+@set nantTaskDir="c:\usr\local\nant-0.85-rc1\bin\tasks\"
+
+@echo installing to %nantTaskDir%
+@echo.
+@echo creating directory...
+@mkdir %nantTaskDir%
+@echo copying files...
+@echo.
+@copy bin\%config%\*.dll %nantTaskDir%
+@if (%config%)==(debug) copy bin\%config%\*.pdb %nantTaskDir%
 
